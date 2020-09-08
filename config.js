216ed96eec9bfaea265b66aaccf2672ac006dfef
TheRoro <39977138+TheRoro@users.noreply.github.com>
2020-09-08 13:48:11 -0500

    Faceboook OAuth Authentication

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '656436121667355',
+        clientSecret: 'eee3202d2bb628dc21e3a363ee53cdf1'
+    }
 }
